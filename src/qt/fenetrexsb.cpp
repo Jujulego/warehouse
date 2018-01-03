035b23f60e7618b56bed52d1dad6495c6adf364b
Nawel Lalioui <nawel-uae@live.fr>
2018-01-02 17:18:22 -0800

    j'ai un peu arrangé :)

--- src/qt/fenetrexsb.cpp
+++ src/qt/fenetrexsb.cpp
@@ -3,17 +3,38 @@
 #include "fenetreniveau.h"
 #include <QString>
 #include <QPushButton>
+#include <QHBoxLayout>
 
 FenetreXSB::FenetreXSB(QString _fichier)
            :m_fichier(_fichier) {
+
+
+    //Taille de la fenêtre
+    setMinimumSize(600, 400);
+    //Titre de la fenêtre
+    setWindowTitle("LEVELS");
+    //Fond du menu
+    this->setStyleSheet("background-color: black;");
+
     FichierXSB fichierxsb(_fichier.toStdString());
 
     for (auto niv : fichierxsb.niveaux()) {
-        m_boutonxsb = new QPushButton(QIcon(":/tileset/perso/haut_01.png"), "NIVEAU", this);
+
+        //Création d'un layout
+        QHBoxLayout *layout = new QHBoxLayout;
+
+        m_boutonxsb = new QPushButton(QIcon(":/tileset/perso/tete_contour.png"), "LEVEL", this);
+        QFont Police("Calibri", 12, QFont::Bold);
+        m_boutonxsb->setFont(Police);
+        m_boutonxsb->setStyleSheet("background-color: yellow;");
+        layout->addWidget(m_boutonxsb);
+           layout->setAlignment(Qt::AlignCenter);
+
         QObject::connect(m_boutonxsb, &QPushButton::clicked, [=] () {
           niv.carte();
           FenetreNiveau* c  = new FenetreNiveau(niv.carte());
           c->show();
+
         });
 
   }
